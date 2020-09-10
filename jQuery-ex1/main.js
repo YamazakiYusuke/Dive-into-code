7c0fe9dd1a239d2c73fcb6878172d81ed2111741
yamazaki yusuke <yusuke38098@gmail.com>
2020-09-10 05:22:28 +0000

    9/10

---
+++ jQuery-ex1/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.tab li').on('click',function(){
+        $('.tab li').removeClass('select');
+        $(this).addClass('select');
+        
+        $('.content li').addClass('hide');
+        let index = $('.tab li').index($(this));
+        console.log(index);
+        $('.content li').eq(index).removeClass('hide');
+    });
+});
